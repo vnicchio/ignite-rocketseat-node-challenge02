61dca06669ac3648eeb573718b3991a3628a4631
Vitor Nicchio Alves <vitor@imaginationmedia.com>
2021-08-03 12:16:52 -0300

    Challenge Completed

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,19 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if(!user) {
+      throw new Error("User does not exist!");
+    }
+
+    if(user.admin === false) {
+      throw new Error("user is not admin!");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
